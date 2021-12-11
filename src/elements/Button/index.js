a9226503bf9f955ce22e5b4c515f3ddc5e1ffb74
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-11 17:14:34 +0700

    Setup propTypes for button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,3 +5,16 @@ import PropTypes from "prop-types";
 export default function Button(props) {
   return <div></div>;
 }
+
+Button.propTypes = {
+  type: PropTypes.oneOf(["button", "link"]), //type dari button akan menerima "button" atau "link"
+  onClick: PropTypes.func, //Memastikan onClick menerima function
+  target: PropTypes.string, // untuk link external biasanya
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isSmall: PropTypes.bool,
+  isLarge: PropTypes.bool,
+  isBlock: PropTypes.bool,
+  hasShadow: PropTypes.bool
+};
