df4367d96ce9df2b540857e0ebc53aaf9d44ddd5
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-12 18:48:54 +0700

    Render IconText in App successful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-
 import "assets/scss/style.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import IconText from "parts/IconText";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <IconText />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
