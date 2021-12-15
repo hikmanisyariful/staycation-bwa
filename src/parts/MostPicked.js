6c40541a3f074ee280718b55fe0c2cc7f8ebbe1a
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-16 03:25:24 +0700

    UI MostPicked with container-grid.scss

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -6,19 +6,21 @@ export default function MostPicked(props) {
   return (
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
-      <div className="row">
-        <div className="col4">
-          <div className="card h-100">Card 1</div>
+      <div className="container-grid">
+        <div className="item column-4 row-2">
+          <div className="card">Card 1</div>
         </div>
-        <div className="col-8">
-          <div className="row mb-3">
-            <div className="col-6">Card 2</div>
-            <div className="col-6">Card 3</div>
-          </div>
-          <div className="row">
-            <div className="col-6">Card 4</div>
-            <div className="col-6">Card 5</div>
-          </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 2</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 3</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 4</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 5</div>
         </div>
       </div>
     </section>
