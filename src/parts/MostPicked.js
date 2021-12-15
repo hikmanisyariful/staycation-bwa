ca34d8bf85d9633d8b5a64472c8a89fb5dd2c4e3
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-16 02:38:11 +0700

    Style : grid-bootstrap

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+// Import
+
 export default function MostPicked(props) {
   return (
-    <div>
-      <h1>Most Picked</h1>
-    </div>
+    <section className="container">
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="row">
+        <div className="col4">
+          <div className="card h-100">Card 1</div>
+        </div>
+        <div className="col-8">
+          <div className="row mb-3">
+            <div className="col-6">Card 2</div>
+            <div className="col-6">Card 3</div>
+          </div>
+          <div className="row">
+            <div className="col-6">Card 4</div>
+            <div className="col-6">Card 5</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
