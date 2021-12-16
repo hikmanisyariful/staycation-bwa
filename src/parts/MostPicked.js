9459e69bc73424c81d38c37756b6302241574507
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-16 07:34:35 +0700

    Logic loop data mostPicked

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -7,21 +7,15 @@ export default function MostPicked(props) {
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
       <div className="container-grid">
-        <div className="item column-4 row-2">
-          <div className="card">Card 1</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 2</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 3</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 4</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 5</div>
-        </div>
+        {props.data.map((item, index) => {
+          return (
+            <div
+              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+            >
+              <div className="card">Card {index + 1}</div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
