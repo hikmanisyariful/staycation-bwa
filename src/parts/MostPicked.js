d3ff774d88eb8459c642a0dcd3fcd9459c2e908b
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-16 16:12:36 +0700

    Fix bug - key props

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -14,6 +14,7 @@ export default function MostPicked(props) {
         {props.data.map((item, index) => {
           return (
             <div
+              key={`mostpicked-${index}`}
               className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
             >
               <div className="card card-featured">
