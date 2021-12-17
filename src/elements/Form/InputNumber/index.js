c73b7412f6bf2882b1ac451bd3a6a264194d01e6
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-18 06:57:15 +0700

    Declaration default props

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -4,9 +4,19 @@ import PropTypes from "prop-types";
 import "./index.scss";
 
 export default function Number(props) {
+  // Destructuring of props
   const { value, placeholder, name, min, max, prefix, suffix } = props;
 
+  // Local state
   const [inputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
 
   return <div></div>;
 }
+
+// Declaration default props
+Number.defaultProps = {
+  min: 1,
+  max: 1,
+  prefix: "",
+  suffix: ""
+};
