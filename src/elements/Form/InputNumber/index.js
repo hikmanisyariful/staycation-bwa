abf1fac91830a94f206815b90ee6d7460f009eba
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-18 06:52:40 +0700

    Make local state of inputValue

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./index.scss";
 
 export default function Number(props) {
   const { value, placeholder, name, min, max, prefix, suffix } = props;
+
+  const [inputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
 
   return <div></div>;
 }
