f00131b2358b8ce1d5f873ae6f1a115a76ddc768
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-17 17:33:51 +0700

    Style: testimonial style

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -23,11 +23,11 @@ export default function Testimony({ data }) {
               src={TestimonyAccent}
               alt="Testimonial Frame"
               className="position-absolute"
-              style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+              style={{ margin: `-30px 0 0 -30px` }}
             />
           </div>
         </div>
-        <div className="col-6">
+        <div className="col">
           <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
           <Star value={data.rate} width={35} height={35} spacing={4}></Star>
           <h5 className="h2 font-weight-light line-height-2 my-3">
@@ -36,14 +36,18 @@ export default function Testimony({ data }) {
           <span className="text-gray-500">
             {data.familyName}, {data.familyOccupation}
           </span>
-          <Button
-            className="btn px-5"
-            style={{ marginTop: 40 }}
-            hasShadow
-            isPrimary
-            type="link"
-            href={`/testimonial/${data._id}`}
-          ></Button>
+          <div>
+            <Button
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              hasShadow
+              isPrimary
+              type="link"
+              href={`/testimonial/${data._id}`}
+            >
+              Read Their Story
+            </Button>
+          </div>
         </div>
       </div>
     </section>
