3d3a79b01e1e1aa12167a4f877441ad4b51679c4
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-17 18:12:35 +0700

    Mockup : footer component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,6 +6,7 @@ import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
 import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 // Import data
 import landingPage from "json/landingPage.json";
@@ -29,6 +30,7 @@ export default class LandingPage extends Component {
         />
         <Categories data={landingPage.categories} />
         <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
---
+++ src/parts/Footer.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+// Import Elements
+import Button from "elements/Button";
+
+// Import component
+import IconText from "parts/IconText";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <IconText />
+            <p className="brand-tagline">
+              We kaboom your beauty holiday instantly and memorable.
+            </p>
+          </div>
+          {/* For Beginners */}
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">For Beginers</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+
+          {/* Explore Us */}
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+
+          {/* Connect Us */}
+          <div className="col-4">
+            <h6 className="mt-2">Connect Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button
+                  type="link"
+                  isExternal
+                  href="mailto:support@staycation.id"
+                >
+                  support@staycation.id
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" isExternal href="tel:+622122081996">
+                  021 - 2208 - 1996
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>Staycation, Kemang, Jakarta</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
