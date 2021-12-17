cc0516829824d7a90e27a5eba9bbd1dcee76cb1b
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-18 06:48:08 +0700

    Destructuring props for InputDate component

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./index.scss";
+
+export default function Number(props) {
+  const { value, placeholder, name, min, max, prefix, suffix } = props;
+
+  return <div></div>;
+}
