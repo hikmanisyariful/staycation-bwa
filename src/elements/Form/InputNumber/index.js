f43e1c7f6219084d8a3c5b4f4cf399e1f294e5b7
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-18 08:07:19 +0700

    Handle onChange function

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -10,6 +10,31 @@ export default function Number(props) {
   // Local state
   const [inputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
 
+  // onChange function
+  const onChange = e => {
+    let value = String(e.target.value);
+    if (prefix) value = value.replace(prefix); // this will remove prefix
+    if (suffix) value = value.replace(suffix); // this will remove suffix
+
+    const patternNumeric = new RegExp("[0-9]*"); // Regex : just receive value from 0 until 9 and * will check all values.
+    const isNumeric = patternNumeric.test(value); // check true/false and .test is function of patternNumeric.
+
+    // +value as sama as Number(value) - This is shorthand to change string to number
+    if (isNumeric && +value <= max && +value >= min) {
+      // Running function which is passed through props.onChange
+      // and passing data of object target to the function which will be further processed in the parent component.
+      props.onChange({
+        target: {
+          name: name,
+          value: value
+        }
+      });
+
+      // This is to view value on UI
+      setInputValue(`${prefix}${value}${suffix}`);
+    }
+  };
+
   return <div></div>;
 }
 
