34c1eb28b055e61d9520bdcc808295ae6ddbec9b
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-19 20:32:59 +0700

    Definition BookingForm component

---
+++ src/parts/BookingForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import PropTypes from "prop-types";
+
+export default class BookingForm extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+BookingForm.propTypes = {
+  itemDetails: PropTypes.object,
+  startBooking: PropTypes.func
+};
