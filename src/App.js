6d1cf69e81861923d9cdf4252133c3ae05a9580c
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-19 19:15:00 +0700

    Setup Details Page component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "assets/scss/style.scss";
 
 // Import Pages
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/properties/:id" element={<DetailsPage />} />
         <Route path="/example" element={<Example />} />
       </Routes>
     </div>
