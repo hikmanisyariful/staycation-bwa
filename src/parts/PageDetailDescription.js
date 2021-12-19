1c91822a032caec7d7bfaad260704a143523679e
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-19 19:57:18 +0700

    Mockup : render UI PageDetailDescription component

---
+++ src/parts/PageDetailDescription.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function PageDetailDescription({ data }) {
+  return (
+    <main>
+      <h4>About the place</h4>
+      <div className="row" style={{ marginTop: 30 }}>
+        {data.featured.map((feature, index) => {
+          return (
+            <div
+              key={`feature-${index}`}
+              className="col-3"
+              style={{ marginBottom: 20 }}
+            >
+              <img
+                src={feature.imageUrl}
+                alt={feature.name}
+                className="d-block mb-2"
+              />{" "}
+              <span>{feature.qty}</span>{" "}
+              <span className="text-gray-500 font-weight-light">
+                {feature.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+}
