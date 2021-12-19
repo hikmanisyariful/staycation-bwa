347dc8ccd650e72e6a0c40c0d8102a7ac0050fb6
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-19 20:36:26 +0700

    Definition local state

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -3,6 +3,20 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class BookingForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        duration: 1,
+        date: {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection"
+        }
+      }
+    };
+  }
+
   render() {
     return <div></div>;
   }
