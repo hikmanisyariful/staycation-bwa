c8c4db7bacd4a9e90482d3e4f37ac3aebeabaa58
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-20 10:20:01 +0700

    Mockup :  render UI BookingForm component

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import PropTypes from "prop-types";
+import { InputNumber, InputDate } from "elements/Form";
+import Button from "elements/Button";
 
 export default class BookingForm extends Component {
   constructor(props) {
@@ -61,7 +63,62 @@ export default class BookingForm extends Component {
   }
 
   render() {
-    return <div></div>;
+    const { data } = this.state;
+    const { itemDetails, startBooking } = this.props;
+
+    return (
+      <div className="card bordered" style={{ padding: "60px 80px" }}>
+        {/* Title Info */}
+        <h4 className="mb-3">Start Booking</h4>
+        <h5 className="h2 text-teal mb-4">
+          Rp. {itemDetails.price}{" "}
+          <span className="text-gary-500 font-weight-light">
+            per {itemDetails.unit}
+          </span>
+        </h5>
+
+        {/* InputNumber */}
+        <label htmlFor="duration">How long you will stay?</label>
+        <InputNumber
+          max={30}
+          suffix=" night"
+          isSuffixPlural
+          onChange={this.updateData}
+          name="duration"
+          value={data.duration}
+        />
+
+        {/* InputDate */}
+        <label htmlFor="date">Pick a date</label>
+        <InputDate onChange={this.updateData} name="date" value={data.date} />
+
+        {/* Info result */}
+        <h6
+          className="text-gray-500 font-weight-light"
+          style={{ marginBottom: 40 }}
+        >
+          You will pay{" "}
+          <span className="text-gray-900">
+            {itemDetails.price * data.duration} USD
+          </span>
+          {" per "}
+          <span className="text-gray-900">
+            {data.duration} {itemDetails.unit}
+          </span>
+        </h6>
+
+        {/* Button */}
+        <Button
+          className="btn"
+          hasShadow
+          isPrimary
+          isBlock
+          onClick={startBooking}
+        >
+          Continue to Booking
+        </Button>
+      </div>
+    );
   }
 }
 
