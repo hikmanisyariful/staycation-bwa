87f50622cdb99d8e817f1cfdff64067963a0c51a
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-22 01:18:10 +0700

    Finish Details page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -6,7 +6,9 @@ import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
-// import Booking from "parts/Booking";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import ItemDetails from "json/itemDetails.json";
 
@@ -35,11 +37,11 @@ export default class DetailsPage extends Component {
             <div className="col-5">
               <BookingForm itemDetails={ItemDetails} />
             </div>
-            {/* <div className="col-5">
-              <Booking itemDetails={ItemDetails} />
-            </div> */}
           </div>
         </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
