5116ba5d355c6952f1aeface3e8096f9518ac683
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-22 10:22:53 +0700

    Mockup : render UI InputFile component

---
+++ src/elements/Form/InputFile/index.js
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+
+export default function File(props) {
+  const {
+    value,
+    placeholder,
+    name,
+    accept,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName
+  } = props;
+
+  const refInputFile = useRef(null);
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-tex">{prepend}</span>
+          </div>
+        )}
+        <input
+          accept={accept}
+          ref={refInputFile}
+          name={name}
+          type="file"
+          className="d-none"
+          value={value}
+          onChange={onChange}
+        />
+        <input
+          onClick={() => refInputFile.current.click()}
+          defaultValue={value}
+          placeholder={placeholder}
+          className={["form-control", inputClassName].join(" ")}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+      {HasError && <span className="error-helper">{hasError}</span>}
+    </div>
+  );
+}
+
+File.defaultProps = {
+  placeholder: "Browse a file"
+};
+
+File.propTypes = {
+  name: PropTypes.string.isRequired,
+  accept: PropTypes.string.isRequired, // untuk mementukan jenis filenya misal: .jpef / .pdf / dst
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  onChange: PropTypes.func.isRequired,
+  prepend: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  append: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  placeholder: PropTypes.string,
+  outerClassName: PropTypes.string,
+  inputClassName: PropTypes.string
+};
